f50c51ea12f313606793b5fd37a633491d10c4e2
josemateus <josemateus.ufma@gmail.com>
2023-07-29 20:07:12 -0300

    tasklets

---
+++ src/test/java/com/batch/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
